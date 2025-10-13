378ea6f74ce344d3e95dab82c9ad09259a380e77
Naeem Akhter <naeem@arnxt.com>
2025-10-13 15:41:46 +0530

    debuggin

--- src/pages/AutoMobileScanner.jsx
+++ src/pages/AutoMobileScanner.jsx
@@ -1,9 +1,15 @@
-import React, { lazy, Suspense, useState } from 'react'
+import React, { lazy, Suspense, useEffect, useState } from 'react'
 const Loader = lazy(() => import('../components/Loader'))
 const TargetImageScanner = lazy(() => import('../components/TargetImageScanner'))
 
 const AutoMobileScanner = () => {
-  const [targetState, setTargetState] = useState({ targetImg: '/autoMobile/auto-mobile-mind-ar.mind', video: "/autoMobile/auto-mobile-video.mp4" })
+  const [targetState, setTargetState] = useState({ targetImg: '/autoMobile/auto-mobile-mind-ar.mind', video: "/autoMobile/auto-mobile-video.mp4" });
+  useEffect(() => {
+    // console.log(navigator.appVersion);
+
+    const deviceName = navigator?.userAgentData?.platform || navigator?.platform;
+    alert(deviceName);
+  }, [])
   return (
     <Suspense fallback={<Loader />}>
       <TargetImageScanner {...targetState} />
